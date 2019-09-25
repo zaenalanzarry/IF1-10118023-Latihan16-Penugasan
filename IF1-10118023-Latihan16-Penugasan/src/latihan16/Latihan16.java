ea711b250e6a223a42aa322a6162c0a25e75c184
Zaenal Anzarry <55571352+zaenalanzarry@users.noreply.github.com>
2019-09-25 14:56:08 +0700

    Add files via upload

---
+++ IF1-10118023-Latihan16-Penugasan/src/latihan16/Latihan16.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package latihan16;
+
+/**
+ * @author Zaenal PC
+ * NAMA              : Zaenal Anzarry
+ * KELAS             : IF-01
+ * NIM               : 10118023
+ * Deskripsi Program : Penugasan
+ */
+
+public class Latihan16 {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 0;
+        
+        System.out.println("Nilai a = " + a);
+        System.out.println("Nilai b = " + b);
+        System.out.println("Nilai c = " + c+"\n");
+        
+        c = a + b;
+        System.out.println("c = a + b = " + c);
+        c += a;
+        System.out.println("c += a = " + c);
+        c -= a;
+        System.out.println("c -= a = " + c);
+        c *= a;
+        System.out.println("c *= a = " + c);
+        a = 10;
+        c = 15;
+        c /= a;
+        System.out.println("c /= a = " + c);
+        a =10;
+        c = 15;
+        c %= a;
+        System.out.println("c %= a = " + c);
+        c <<= 2;
+        System.out.println("c <<= 2 = " + c);
+        c >>= 2;
+        System.out.println("c >>= 2 = " + c);
+        c >>= 2;
+        System.out.println("c >>= a = " + c);
+        c &= a;
+        System.out.println("c &= 2 = " + c);
+        c ^= a;
+        System.out.println("c ^= a = " + c);
+        c |= a;
+        System.out.println("c |= a = " + c);
+        
+    }
+    
+}
